843951751c8a3e81e0d398ffe69f328d1da02ae4
XxWR41THxX <139513634+XxWR41THxX@users.noreply.github.com>
2023-11-21 12:41:42 -0800

    Create Iterate.py
    Just a fancier script for all things that don't take wordlists naturally.

---
+++ Iterate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from termcolor import colored  # You may need to install the termcolor module
+
+def run_tool(command, filepath):
+    output_folder = "scan_results"
+    os.makedirs(output_folder, exist_ok=True)
+
+    with open(filepath, "r") as file:
+        for line in file:
+            domain = line.strip()
+            output_file = f"{output_folder}/{command.replace(' ', '_')}_{domain.replace('.', '_')}.txt"
+
+            try:
+                process = subprocess.Popen(command.split() + [domain], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1, universal_newlines=True)
+
+                for line in process.stdout:
+                    print(colored(line.strip(), "green"))
+
+                for line in process.stderr:
+                    print(colored(line.strip(), "red"))
+
+                process.wait()
+
+                # You may want to save the output to a file if the tool doesn't do it automatically
+                with open(output_file, "w") as output:
+                    output.write(process.stdout.read())
+            except subprocess.CalledProcessError as e:
+                print(colored(f"Error running {command} for {domain}: {e.stderr.strip()}", "red"))
+
+def main():
+    command = input("Enter the command to run (e.g., nmap -p- -T4): ")
+    filepath = input("Enter the file path containing domains: ")
+
+    run_tool(command, filepath)
+
+if __name__ == "__main__":
+    main()
